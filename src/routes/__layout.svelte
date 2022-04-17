2174b5cdb1553b6320c6fc69ffe0b99bbf536b85
leovoon <voonlihhaur@gmail.com>
2022-04-17 17:40:55 +0800

    add app.css and dynamic title

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,24 +1,35 @@
+<script>
+	import MetaTitle from '$lib/MetaTitle.svelte';
+	import '../app.css';
+	
+</script>
+
+<MetaTitle title="Leo's code snippets" />
+
 <div class="wrapper">
-    <slot />
-  </div>
-  <footer>by leovoon</footer>
-  
-  <style>
-    .wrapper {
-      width: 50vw;
-      margin: 0 auto;
-    }
-    :global(.more-link) {
-      display: none;
-    }
-    :global(img) {
-      max-width: 100%;
-    }
-    @media (max-width: 768px) {
-      .wrapper {
-        width: 90vw;
-      }
-    
-    }
-   
-  </style>
+	<slot />
+</div>
+<footer>
+	Built by leovoon.
+	<a href="/">Home</a>
+	|
+	<a href="/login">Login</a>
+</footer>
+
+<style>
+	.wrapper {
+		width: 50vw;
+		margin: 0 auto;
+	}
+	:global(.more-link) {
+		display: none;
+	}
+	:global(img) {
+		max-width: 100%;
+	}
+	@media (max-width: 768px) {
+		.wrapper {
+			width: 90vw;
+		}
+	}
+</style>
