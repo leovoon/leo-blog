5d8a192698437f5aac34c496a205fc8fa30cbc59
leovoon <voonlihhaur@gmail.com>
2022-04-26 22:17:35 +0800

    add 404 error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	/** @type {import('@sveltejs/kit').Load} */
+	export function load({ error, status }) {
+		return {
+			props: {
+				title: `${status}`
+			}
+		};
+	}
+</script>
+
+<script>
+	import MetaTitle from '$lib/MetaTitle.svelte';
+
+	export let title;
+</script>
+
+<MetaTitle title="Opps.." />
+<h1>
+	{title}
+</h1>
+
+<p style="text-align:center;">This page does not appear on the website map.</p>
