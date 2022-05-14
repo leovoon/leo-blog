9fdad73494cf34b53d6bc99eaa37ea03035d6843
leovoon <voonlihhaur@gmail.com>
2022-05-14 22:55:22 +0800

    add category badge colors

--- src/lib/CategoryBadge.svelte
+++ src/lib/CategoryBadge.svelte
@@ -1,15 +1,21 @@
 <script>
 	export let category;
+	let color;
+	$: {
+		if (category === 'Svelte') color = 'rgb(232, 146, 135)';
+		if (category === 'JavaScript') color = '#E8B882';
+		if (category === 'CSS') color = '#4AA6F0';
+	}
 </script>
 
-<badge>{category}</badge>
+<badge style:--badgeBgColor={color}>{category}</badge>
 
 <style>
 	badge {
 		font-size: 0.8rem;
 		border-radius: 13px;
 		padding: 0.3rem 1rem;
-		background-color: rgb(232, 146, 135);
+		background-color: var(--badgeBgColor);
 		color: var(--pure-white);
 	}
 </style>
