9b89b04cf3c622166c038356e9ba95de674b12c4
leovoon <voonlihhaur@gmail.com>
2022-05-16 23:12:31 +0800

    add home meta tags

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,10 @@
 <script>
-	import MetaTitle from '$lib/MetaTitle.svelte';
 	import 'nprogress/nprogress.css';
 	import '../app.css';
 	import NProgress from 'nprogress';
 	import { navigating, page } from '$app/stores';
 	import SearchPost from '$lib/SearchPost.svelte';
+	import { MetaTags } from 'svelte-meta-tags';
 
 	// Top loading bar
 	NProgress.configure({
@@ -19,9 +19,32 @@
 
 	let searchText = '';
 	let wpLoginUrl = 'https://wordpress.com/log-in';
+	let description = 'Helllo web frontend, I wrote about Svelte, CSS and JavaScript.';
+	let title = $page.title;
+	let url = $page.url;
 </script>
 
-<MetaTitle title="Leo's code snippets" />
+<MetaTags
+	title="lecs"
+	titleTemplate="%s | Leo's FE Code Snippet"
+	{description}
+	canonical="https://leocs.netlify.app"
+	openGraph={{
+		url,
+		title,
+		description,
+		site_name: 'Lecs'
+	}}
+	twitter={{
+		handle: '@handle',
+		site: '@site',
+		cardType: 'summary_large_image',
+		title,
+		description,
+		image: 'https://generative-placeholders.glitch.me/image?width=100&height=100',
+		imageAlt: title
+	}}
+/>
 <SearchPost bind:value={searchText} />
 
 <main>
